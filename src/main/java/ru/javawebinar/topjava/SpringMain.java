6e44b20323ea6dfba2b5c0402d3ad4164ed362e2
Aleksey Makaenko <makalex74@yandex.ru>
2021-07-18 20:11:17 +0300

    Apply 3_03_HW2_meal_layers.patch

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -4,23 +4,33 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.repository.UserRepository;
-import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.web.meal.MealRestController;
+import ru.javawebinar.topjava.web.user.AdminRestController;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
 import java.util.Arrays;
+import java.util.List;
 
 public class SpringMain {
     public static void main(String[] args) {
-        ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
-        System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+        // java 7 automatic resource management (ARM)
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+            System.out.println();
 
-//        UserRepository userRepository = (UserRepository) appCtx.getBean("inmemoryUserRepository");
-        UserRepository userRepository = appCtx.getBean(UserRepository.class);
-        userRepository.getAll();
-
-        UserService userService = appCtx.getBean(UserService.class);
-        userService.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
-
-        appCtx.close();
+            MealRestController mealController = appCtx.getBean(MealRestController.class);
+            List<MealTo> filteredMealsWithExcess =
+                    mealController.getBetween(
+                            LocalDate.of(2020, Month.JANUARY, 30), LocalTime.of(7, 0),
+                            LocalDate.of(2020, Month.JANUARY, 31), LocalTime.of(11, 0));
+            filteredMealsWithExcess.forEach(System.out::println);
+            System.out.println();
+            System.out.println(mealController.getBetween(null, null, null, null));
+        }
     }
 }
