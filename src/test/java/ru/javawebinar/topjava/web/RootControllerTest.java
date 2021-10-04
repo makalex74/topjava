d19ae2d47ed6c67f2ba846083dd55aba0c0cf520
makalex74 <makalex74@yandex.ru>
2021-10-04 17:31:55 +0300

    HW07 1.1 finish

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -10,27 +10,28 @@ import java.util.List;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static ru.javawebinar.topjava.MealTestData.*;
-//import static ru.javawebinar.topjava.UserTestData.*;
+import static ru.javawebinar.topjava.MealTestData.meals;
+import static ru.javawebinar.topjava.UserTestData.*;
+import static ru.javawebinar.topjava.util.MealsUtil.getTos;
 
 class RootControllerTest extends AbstractControllerTest {
 
-//    @Test
-//    void getUsers() throws Exception {
-//        perform(get("/users"))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(view().name("users"))
-//                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
-//                .andExpect(model().attribute("users",
-//                        new AssertionMatcher<List<User>>() {
-//                            @Override
-//                            public void assertion(List<User> actual) throws AssertionError {
-//                                MATCHER.assertMatch(actual, admin, user);
-//                            }
-//                        }
-//                ));
-//    }
+    @Test
+    void getUsers() throws Exception {
+        perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("users"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
+                .andExpect(model().attribute("users",
+                        new AssertionMatcher<List<User>>() {
+                            @Override
+                            public void assertion(List<User> actual) throws AssertionError {
+                                MATCHER.assertMatch(actual, admin, user);
+                            }
+                        }
+                ));
+    }
 
     @Test
     void getMeals() throws Exception {
@@ -39,13 +40,6 @@ class RootControllerTest extends AbstractControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("meals"))
                 .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
-                .andExpect(model().attribute("meals",
-                        new AssertionMatcher<List<Meal>>() {
-                            @Override
-                            public void assertion(List<Meal> actual) throws AssertionError {
-                                MATCHER.assertMatch(actual, meal7, meal6, meal5, meal4, meal3, meal2, meal1);
-                            }
-                        }
-                ));
+                .andExpect(model().attribute("meals", getTos(meals, SecurityUtil.authUserCaloriesPerDay())));
     }
 }
